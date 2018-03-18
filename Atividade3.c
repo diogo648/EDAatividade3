d0a1172c1369abed0a6e45ed85eb85cb042bdb42
diogo648 <diogocortez@live.com>
2018-03-18 16:16:14 -0300

    inclusao de ifs

--- Atividade3.c
+++ Atividade3.c
@@ -13,10 +13,16 @@ struct tno{
 }
 
 
-int insereAluno(int ra, char nomeAluno[50], float nota, no **proxAluno){
+
+
+void insereAluno(int ra, char nomeAluno[50], float nota){
+
 
 	
-	
+}
+
+void exibeDados(){
+
 }
 
 
@@ -24,37 +30,72 @@ int insereAluno(int ra, char nomeAluno[50], float nota, no **proxAluno){
 int main(){
 
 
-int opcao;
+int opcao=0;
 int ra;
 char nomeAluno[50];
 float nota;
 
 
 
-while(opcao)
-printf("\n MENU \n");
-printf("1. Inserir o cadastro de um aluno \n");
-printf("2. Buscar aluno por RA \n");
-printf("3. Buscar aluno por Nome \n");
-printf("4. Exibir dados de todos os alunos cadastrados \n");
-printf("5. Sair \n")
-scanf("&d",opcao);
+
+	while(opcao != -1)
+	{
+
+		printf("\n MENU \n");
+		printf("1. Inserir o cadastro de um aluno \n");
+		printf("2. Buscar aluno por RA \n");
+		printf("3. Buscar aluno por Nome \n");
+		printf("4. Exibir dados de todos os alunos cadastrados \n");
+		printf("5. Sair \n")
+		scanf("&d",opcao);
 
 
+		
+		if(opcao == 1)
+		{
 
-if(opcao == 1){
+			printf("Digite o RA do aluno:");
+			scanf("%d",ra);
 
-	printf("Digite o RA do aluno:");
-	scanf("%d",ra);
+			printf("Digite o nome do aluno:");
+			gets(nomeAluno);
 
-	printf("Digite o nome do aluno:");
-	gets(nomeAluno);
+			printf("Digite a nota do aluno:");
+			gets("%f", nota);
+			
+			//Insere Aluno
+			insereAluno(ra,nomeAluno,nota);
+		}
 
-	printf("Digite a nota do aluno:");
-	gets("%f", nota);
+		if(opcao == 2)
+		{
 
-	insereAluno(ra,nomeAluno,nota);
-}
+			printf("Digite o RA do aluno:");
+		}
+		
+		if(opcao == 3)
+		{
+			printf("Digite o nome do aluno:");
+		}
+
+		if(opcao == 4)
+		{
+			
+		}
+
+		if(opcao == 5)
+		{
+			opcao = -1;
+		}
+
+		else
+		{
+			printf("Opcao Invalida!\n");
+		}
+
+	}
+
+
 
 
 
