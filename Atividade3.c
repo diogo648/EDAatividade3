5533ace141adb8a6fc65021edde70efdf52e432f
diogo648 <diogocortez@live.com>
2018-03-19 14:47:34 -0300

    atvidade3. c com a funcao insereOrdenado

--- Atividade3.c
+++ Atividade3.c
@@ -1,27 +1,76 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include<string.h>
 
 
-typedef tno no;
+
+typedef struct tno no;
 
 struct tno{
 
 	int ra;
 	char nomeAluno[50];
 	float nota;
-	struct no *prox;
+	struct tno *prox;
+};
+
+
+no *insereAluno(int ra, char nomeAluno[50], float nota){
+
+no *aux = (no *) malloc(sizeof(no));
+
+aux->ra = ra;
+strcpy(aux->nomeAluno,nomeAluno);
+aux->nota = nota;
+aux-> prox = NULL;
+
+return aux;
+
+
+}
+
+no *insereOrdenado(no *lista, no *novaLista)
+{
+	no *temp = NULL, *temp1 = lista;
+
+	if (temp1 == NULL)
+	{
+		lista = novaLista;//Se a lista está vazia, os novos dados são a lista
+	}
+	else
+	{
+		while (temp1 != NULL && novaLista->ra > temp1->ra)//Encontra a posição correta na lista
+		{
+			temp = temp1;
+			temp1 = temp1->prox;
+		}
+
+		if (temp == NULL)//Insere na posição correta
+		{
+			novaLista->prox = lista;
+			lista = novaLista;
+		}
+		else
+		{
+			novaLista->prox = temp1;
+			temp->prox = novaLista;
+		}
+	}
+
+	return lista;//Retorna a lista ordenada
 }
 
 
 
+void exibeDados(no *dadosAlunos){
 
-void insereAluno(int ra, char nomeAluno[50], float nota){
+while(dadosAlunos)
+{
 
+	printf("Aluno: %s ..... RA: %d ..... Nota: %f \n",dadosAlunos->nomeAluno,dadosAlunos->ra,dadosAlunos->nota);
 
-	
+	dadosAlunos = dadosAlunos -> prox;
 }
-
-void exibeDados(){
 
 }
 
@@ -35,19 +84,20 @@ int ra;
 char nomeAluno[50];
 float nota;
 
+no *alunos= NULL, *aux;
 
 
+no *lista=NULL; //Lista vazia
 
-	while(opcao != -1)
+	while(opcao!= -1)
 	{
-
 		printf("\n MENU \n");
 		printf("1. Inserir o cadastro de um aluno \n");
 		printf("2. Buscar aluno por RA \n");
 		printf("3. Buscar aluno por Nome \n");
 		printf("4. Exibir dados de todos os alunos cadastrados \n");
-		printf("5. Sair \n")
-		scanf("&d",opcao);
+		printf("5. Sair \n");
+		scanf("%d",&opcao);
 
 
 		
@@ -55,22 +105,32 @@ float nota;
 		{
 
 			printf("Digite o RA do aluno:");
-			scanf("%d",ra);
+			scanf("%d",&ra);
+			__fpurge(stdin);
 
 			printf("Digite o nome do aluno:");
 			gets(nomeAluno);
 
 			printf("Digite a nota do aluno:");
-			gets("%f", nota);
+			scanf("%f",&nota);
+			__fpurge(stdin);			
 			
 			//Insere Aluno
-			insereAluno(ra,nomeAluno,nota);
+			aux = insereAluno(ra,nomeAluno,nota);
+
+			if(aux == NULL){
+
+				return -1;
+			}
+
+			alunos = insereOrdenado(alunos,aux);
 		}
 
 		if(opcao == 2)
 		{
 
 			printf("Digite o RA do aluno:");
+			scanf("%d",&ra);
 		}
 		
 		if(opcao == 3)
@@ -80,7 +140,7 @@ float nota;
 
 		if(opcao == 4)
 		{
-			
+			exibeDados(alunos);
 		}
 
 		if(opcao == 5)
@@ -88,13 +148,8 @@ float nota;
 			opcao = -1;
 		}
 
-		else
-		{
-			printf("Opcao Invalida!\n");
-		}
 
 	}
-
 
 
 
